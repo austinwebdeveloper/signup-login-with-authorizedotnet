028e07103e35877c5d098436c2c013d6d8ae076a
nuevesolutions <atxwebdesigner1@gmail.com>
2018-08-22 09:20:56 +0530

    new changes

--- common/models/users.js
+++ common/models/users.js
@@ -267,5 +267,156 @@ Users.remoteMethod('cancelSubscription',{
 ]
  });
 
+
+ Users.normalizeCredentials = function(credentials, realmRequired, realmDelimiter) {
+  var query = {};
+  credentials = credentials || {};
+  if (!realmRequired) {
+    if (credentials.email) {
+      query.email = credentials.email.toLowerCase();
+    } else if (credentials.username) {
+      query.username = credentials.username;
+    }
+  } else {
+    if (credentials.realm) {
+      query.realm = credentials.realm;
+    }
+    var parts;
+    if (credentials.email) {
+      parts = splitPrincipal(credentials.email, realmDelimiter);
+      query.email = parts[1];
+      if (parts[0]) {
+        query.realm = parts[0];
+      }
+    } else if (credentials.username) {
+      parts = splitPrincipal(credentials.username, realmDelimiter);
+      query.username = parts[1];
+      if (parts[0]) {
+        query.realm = parts[0];
+      }
+    }
+  }
+  return query;
+};
+
+
+
+/**
+*
+* @param {object} data
+* @param {Function(Error, object)} callback
+*/
+/* User.login = function (credentials, include, callback) {
+// Invoke the default login function
+User.find({where: {and: [{email: credentials.email }, {username: 'svl'}]}},
+function (err, posts) {
+console.log(posts)
+callback(null, posts)
+});
+};
+*/
+Users.login = function(credentials, include, fn) {
+var self = this;
+if (typeof include === 'function') {
+fn = include;
+include = undefined;
+}
+
+fn = fn || utils.createPromiseCallback();
+
+include = (include || '');
+if (Array.isArray(include)) {
+include = include.map(function(val) {
+  return val.toLowerCase();
+});
+} else {
+include = include.toLowerCase();
+}
+
+var realmDelimiter;
+// Check if realm is required
+var realmRequired = !!(self.settings.realmRequired ||
+self.settings.realmDelimiter);
+if (realmRequired) {
+realmDelimiter = self.settings.realmDelimiter;
+}
+var query = self.normalizeCredentials(credentials, realmRequired,
+realmDelimiter);
+
+if (realmRequired && !query.realm) {
+var err1 = new Error(g.f('{{realm}} is required'));
+err1.statusCode = 400;
+err1.code = 'REALM_REQUIRED';
+fn(err1);
+return fn.promise;
+}
+if (!query.email && !query.username) {
+var err2 = new Error(g.f('{{username}} or {{email}} is required'));
+err2.statusCode = 400;
+err2.code = 'USERNAME_EMAIL_REQUIRED';
+fn(err2);
+return fn.promise;
+}
+
+self.findOne({where: query}, function(err, user) {
+  var defaultError = new Error(g.f('login failed'));
+defaultError.statusCode = 401;
+defaultError.code = 'LOGIN_FAILED';
+
+function tokenHandler(err, token) {
+  if (err) return fn(err);
+  if (Array.isArray(include) ? include.indexOf('user') !== -1 : include === 'user') {
+    // NOTE(bajtos) We can't set token.user here:
+    //  1. token.user already exists, it's a function injected by
+    //     "AccessToken belongsTo User" relation
+    //  2. ModelBaseClass.toJSON() ignores own properties, thus
+    //     the value won't be included in the HTTP response
+    // See also loopback#161 and loopback#162
+    token.__data.user = user;
+  }
+  fn(err, token);
+}
+
+if (err) {
+  debug('An error is reported from User.findOne: %j', err);
+  fn(defaultError);
+} else if (user) {
+  user.hasPassword(credentials.password, function(err, isMatch) {
+    if (err) {
+      debug('An error is reported from User.hasPassword: %j', err);
+      fn(defaultError);
+    } else if (isMatch) {
+      if (self.settings.emailVerificationRequired && !user.emailVerified) {
+        // Fail to log in if email verification is not done yet
+        debug('User email has not been verified');
+        err = new Error(g.f('login failed as the email has not been verified'));
+        err.statusCode = 401;
+        err.code = 'LOGIN_FAILED_EMAIL_NOT_VERIFIED';
+        err.details = {
+          userId: user.id,
+        };
+        fn(err);
+      } else {
+        if (user.createAccessToken.length === 2) {
+          user.createAccessToken(credentials.ttl, tokenHandler);
+
+        } else {
+          user.createAccessToken(credentials.ttl, credentials, tokenHandler);
+        }
+      }
+    } else {
+      debug('The password is invalid for user %s', query.email);
+      fn(defaultError);
+    }
+  });
+} else {
+  debug('No matching record is found for user %s', query.email );
+  fn(defaultError);
+}
+});
+return fn.promise;
+};
+
+
  };
 
